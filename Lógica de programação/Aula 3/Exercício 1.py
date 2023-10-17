4d391369455cb10261d0f83624100addd369c73a
Matheus Nani Pacífico <148264422+Matheusnanipac@users.noreply.github.com>
2023-10-17 15:07:08 -0300

    Exercício 1.py
    Primeiro exercício da aula 3

---
+++ Lógica de programação/Aula 3/Exercício 1.py
@@ -0,0 +1,10 @@
+#Codigo para descobrir se o X e maior menor ou igual a Y
+x = int(input("Digite o valor de x"))
+y = int(input("Digite o valor de y"))
+
+if x > y:
+  print("x é maior do que y")
+elif x < y:
+  print("y é maior do que x")
+else:
+  print("x é igual a y")
