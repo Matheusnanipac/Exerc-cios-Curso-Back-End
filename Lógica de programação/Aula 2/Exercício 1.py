6fb99dd97b87f73e9aa8f21f0ea01fab9f3d8181
Matheus Nani Pacífico <148264422+Matheusnanipac@users.noreply.github.com>
2023-10-17 15:02:29 -0300

    Exercício 1.py
    Pasta dos exercícios da aula 2

---
+++ Lógica de programação/Aula 2/Exercício 1.py
@@ -0,0 +1,17 @@
+#Codigo para soma(Concatenar) de 2 valores inteiros
+
+num1 = int(input("Digite o primeiro número:"))
+num2 = int(input("Digite o segundo número:"))
+
+soma = num1 + num2
+
+print(f"O resultado da soma entre {num1} e {num2} é {soma}")
+
+#Codigo para a soma de 2 valores quebrados
+
+num1 = float(input("Digite o primeiro número:"))
+num2 = float(input("Digite o segundo número"))
+
+soma = num1 + num2
+
+print(f"O resultado da soma entre {num1} e {num2} é {soma}")
