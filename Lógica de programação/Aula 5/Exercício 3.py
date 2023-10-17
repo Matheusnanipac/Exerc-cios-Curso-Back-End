4acc51cdebfe769c27a97b452f44edc4ef6b3da9
Matheus Nani Pacífico <148264422+Matheusnanipac@users.noreply.github.com>
2023-10-17 15:39:52 -0300

    Exercício 3.py
    Terceiro exercício

---
+++ Lógica de programação/Aula 5/Exercício 3.py
@@ -0,0 +1,16 @@
+#Define o maior numero do array!
+Lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+Maior_Elemento = 0
+
+for Num in Lista:
+    if Num > Maior_Elemento:
+        Maior_Elemento = Num
+
+print(Maior_Elemento)
+
+#Define o maior valor do array!
+Lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+Maior_Elemento = max(Lista)
+
+print(Maior_Elemento)
